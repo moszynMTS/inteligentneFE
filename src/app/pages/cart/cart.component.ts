721c84cd00fdfb319bd10fc10bb2945d50595c54
Szymon <szmsmi@gmail.com>
2025-01-06 20:08:27 +0100

    placeholders for testing

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -15,10 +15,10 @@ export class CartComponent implements OnInit {
   totalPrice: number = 0;
   searchForm: FormGroup; 
   items: any[] = [
-    { id:1, title: 'Product 1', price: 19.99 },
-    { id:2, title: 'Product 2', price: 29.99 },
-    { id:3, title: 'Product 3', price: 39.99 },
-    { id:4, title: 'Product 4', price: 39.99 },
+    { id:1, title: 'Product 1', price: 19.99, url: 'https://placehold.jp/150x150.png' },
+    { id:2, title: 'Product 2', price: 29.99, url: 'https://placehold.jp/150x150.png' },
+    { id:3, title: 'Product 3', price: 39.99, url: 'https://placehold.jp/150x150.png' },
+    { id:4, title: 'Product 4', price: 39.99, url: 'https://placehold.jp/150x150.png' },
   ];
   constructor(
     private snackBar: MatSnackBar,
